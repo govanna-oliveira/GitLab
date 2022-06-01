c6b136519c5e3ebaa7bf7126a2fe4168702c01e2
Geovanna <geovannaoliveira169@gmail.com>
2022-06-01 11:07:37 -0300

    operacao subtrair

--- Program.cs
+++ Program.cs
@@ -5,5 +5,10 @@ double n2 = Convert.ToDouble (args[2]);
 //dotnet run -- Somar 1,3 2,5
 if(operacao == "Somar")
 {
-    Console.WriteLine(n1+n2);
+    Console.WriteLine(n1 + n2);
+}
+
+if(operacao == "Subtrair")
+{
+    Console.WriteLine(n1 - n2);
 }
