6df992dbefd76b57141092b760b82dfc922111ae
Geovanna <geovannaoliveira169@gmail.com>
2022-06-01 11:04:00 -0300

    operacao somar

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string operacao = args[0];
+double n1 = Convert.ToDouble (args[1]);
+double n2 = Convert.ToDouble (args[2]);
+
+//dotnet run -- Somar 1,3 2,5
+if(operacao == "Somar")
+{
+    Console.WriteLine(n1+n2);
+}
